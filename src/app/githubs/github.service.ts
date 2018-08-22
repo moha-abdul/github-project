323ccfd4440a98defd9e0956ae11b131ad74a934
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-22 15:41:23 +0300

    import key and url from environment into service

--- src/app/githubs/github.service.ts
+++ src/app/githubs/github.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  apiKey = environment.apiKey;
+  api_Url = environment.apiUrl;
 
   constructor(private http: HttpClientModule) { }
 }
