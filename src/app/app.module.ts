44210d99c16ffb4a786d83ce53b98542492f9bc8
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-22 15:21:59 +0300

    Import Httpclientmodule into module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { GithubService } from './githubs/github.service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GithubNavComponent } from './github-nav/github-nav.component';
@@ -11,7 +12,8 @@ import { GithubNavComponent } from './github-nav/github-nav.component';
     GithubNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [GithubService],
   bootstrap: [AppComponent]
