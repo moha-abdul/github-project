f5ddd9035e3feb8cd5311fabb3529aee43bac97d
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-22 15:32:20 +0300

    Import httpclientmodule and inject in github service

--- src/app/githubs/github.service.ts
+++ src/app/githubs/github.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private http: HttpClientModule) { }
 }
