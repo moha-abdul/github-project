db2c1d00f6a0754e063bdaf03777c4cf6497bf43
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-22 17:30:06 +0300

    Define properties

--- src/app/githubs/github.service.ts
+++ src/app/githubs/github.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { environment } from '../../environments/environment';
 import { Users } from '../users';
 import { Repos } from '../repos';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
-  apiKey = environment.apiKey;
-  api_Url = environment.apiUrl;
-
+  user: Users;
+  apiKey: string = environment.apiKey;
+  api_Url: string = environment.apiUrl;
+  repos$
   constructor(private http: HttpClientModule) { }
-  searchGithub(username) {}
+  searchGithubUser(username) {}
 }
