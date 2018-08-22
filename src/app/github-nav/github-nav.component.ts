3f19c75038e9974fff7cae9c8417ae03edc11df8
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-22 18:22:35 +0300

    Inject github service into github-nav

--- src/app/github-nav/github-nav.component.ts
+++ src/app/github-nav/github-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../githubs/github.service';
 
 @Component({
   selector: 'app-github-nav',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public githubservice: GithubService) { }
 
   ngOnInit() {
   }
