08d71e23c842c1bd880939e5bba910d74635bd36
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-23 08:58:07 +0300

    Add search function for users

--- src/app/github-nav/github-nav.component.ts
+++ src/app/github-nav/github-nav.component.ts
@@ -4,13 +4,28 @@ import { GithubService } from '../githubs/github.service';
 @Component({
   selector: 'app-github-nav',
   templateUrl: './github-nav.component.html',
-  styleUrls: ['./github-nav.component.css']
+  styleUrls: ['./github-nav.component.css'],
+  providers: [GithubService]
 })
 export class GithubNavComponent implements OnInit {
 
   constructor(public githubservice: GithubService) { }
 
+  searchUser($input) {
+    if ($input.target.value === '') {
+
+    } else {
+      this.githubservice.searchGithubUser($input.target.value);
+      // this.githubservice.reposRequested($key.target.value)
+    }
+
+
+    console.log($input.target.value);
+
+  }
+
   ngOnInit() {
+    // this.githubservice.searchGithubUser('moha-abdul');
   }
 
 }
